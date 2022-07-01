4349970aa9a77078878d8b0896531fbd941a99e0
Kioyar <ehikioyaoriaifo@gmail.com>
2022-06-30 17:58:46 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to be called
+ * return:0 for the int
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,/n");
+	return (0);
+}
